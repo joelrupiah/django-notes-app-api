1902b4150c9e58a36763f50fc2a54a76692fe4c1
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-07-20 10:41:04 +0000

    first commit

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+# create all endpoints
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoutes, name='routes'),
+    path('notes/', views.getNotes, name='notes'),
+    path('notes/<str:pk>', views.getNote, name='note')
+]
---
+++ api/views.py
@@ -0,0 +1,29 @@
+import imp
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        # create a db
+    ]
+
+    return Response(routes) # safe returns more data rather than only python dict
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
